f0c2fbf87bd84f2d5509e71e8099fd8a69ccb690
manish mandal <76776194mm@gmail.com>
2022-08-28 20:40:21 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-const InputArea=document.getElementById('email');
+const inputArea=document.getElementById('email');
+const submitBtn=document.querySelector('.arrow-logo-block');
+const errorIcon=document.querySelector('.error-icon');
+const errorMsge=document.querySelector('.error-msge');
+
+const emailPattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+submitBtn.addEventListener('click',function(){
+    if(!inputArea.value.match(emailPattern)){
+         inputArea.style.border="2px solid red";
+         errorIcon.classList.remove('hidden');
+         errorMsge.textContent="Please provide a valid email";
+    }
+
+   
+});
+// console.log(2);
