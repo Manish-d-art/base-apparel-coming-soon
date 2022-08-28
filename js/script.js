cabd3d581616166064902c22be87bde7badc3f9b
manish mandal <76776194mm@gmail.com>
2022-08-28 22:24:41 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -6,15 +6,25 @@ const errorMsge=document.querySelector('.error-msge');
 const emailPattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
-submitBtn.addEventListener('click',function(){
+submitBtn.addEventListener('click',function(e){
+    e.preventDefault();
+
     submitBtn.style.backgroundImage="linear-gradient(135deg, hsl(0deg, 80%, 86%), hsl(0deg 33% 85%))";
 
-    if(!inputArea.value.match(emailPattern)){
+    if(inputArea.value === ""){
+        errorMsge.textContent="Email cannot be empty";
+        inputArea.style.border="2px solid red";
+        errorIcon.classList.remove('hidden');
+
+    }
+
+    else if(!inputArea.value.match(emailPattern)){
          inputArea.style.border="2px solid red";
          errorIcon.classList.remove('hidden');
          errorMsge.textContent="Please provide a valid email";
          inputArea.placeholder="manish";
     }
+    
     else{
         inputArea.style.border="2px solid green";
     }
@@ -22,4 +32,3 @@ submitBtn.addEventListener('click',function(){
 
    
 });
-// console.log(2);
